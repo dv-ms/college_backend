01855e541af0a88a9dce802b8d1ee05c952bc278
Manav Sethi <dev.manav.sethi@gmail.com>
2019-10-19 15:43:43 +0530

    removed db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-100.times do
-    Department.create(name: Faker::Company.unique.industry, hod_id: Faker::Number.unique.within(range: 1..500))
-end
